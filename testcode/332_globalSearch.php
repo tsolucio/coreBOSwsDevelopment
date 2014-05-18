361b2a7de5c5b58b1a0182d1c046e7c82a4a70ae
Joe Bordes <joe@tsolucio.com>
2014-05-18 13:35:08 +0200

    test global search functionality

---
+++ testcode/332_globalSearch.php
@@ -0,0 +1,40 @@
+<?php
+//doglobalsearch request must be POST request.
+$restrictionsID=json_encode(array(
+			'userId'=>$cbUserID,
+			'accountId'=>'11x4',
+			'contactId'=>'12x30'
+));
+$query = 'mary';
+$params = array("sessionName"=>$cbSessionID,
+				"operation"=>'getSearchResults', "query"=>$query,
+				"search_onlyin"=>'','restrictionids'=>$restrictionsID);
+//Create must be POST Request.
+$response = $httpc->send_post_data($cbURL, $params, true);
+$dmsg.= debugmsg("Raw response (json) unifiedsearch",$response);
+
+//decode the json encode response from the server.
+$jsonResponse = json_decode($response,true);
+$dmsg.= debugmsg("Webservice response unifiedsearch",$jsonResponse);
+
+if($jsonResponse['success']==false) {
+	$dmsg.= debugmsg('unifiedsearch failed:'.$jsonResponse['error']['message']);
+	echo 'unifiedsearch failed!';
+} else {
+	//Show the results
+	$rdos = unserialize($jsonResponse['result']);
+	$nrdos = count($rdos);
+	echo "<b>SEARCH_CRITERIA: $query</b><br>";
+	echo "<b>Results: $nrdos</b><br>";
+	if ($nrdos>0)
+	  echo "<table class='table table-striped small table-condensed'>";
+	  foreach ($rdos as $modrdo) {
+		echo "<tr><td>";
+		foreach($modrdo as $fld) {
+			echo "$fld |";
+		}
+		echo "</td></tr>";
+	  }
+	  echo "</table>";
+}
+?>
