1bee12447d218f6222465c0a972c79e435a786b5
Joe Bordes <joe@tsolucio.com>
2018-12-07 09:56:01 +0100

    (testcode) Execute Workflow

---
+++ testcode/500_ExecuteWorkflow.php
@@ -0,0 +1,24 @@
+<?php
+//sessionId is obtained from loginResult.
+$params = array(
+	"sessionName"=>$cbSessionID,
+	"operation"=>'ExecuteWorkflow',
+	"workflow"=>'27',
+	'entities'=>'["17x2642"]',
+);
+//Create must be POST Request.
+$response = $httpc->send_post_data($cbURL, $params, true);
+$dmsg.= debugmsg("Raw response (json) Update",$response);
+
+//decode the json encode response from the server.
+$jsonResponse = json_decode($response, true);
+$dmsg.= debugmsg("Webservice response Update",$jsonResponse);
+
+if($jsonResponse['success']==false) {
+	$dmsg.= debugmsg('failed:'.$jsonResponse['error']['message']);
+	echo 'failed!';
+} else {
+	$vals = $jsonResponse['result'];
+	var_dump($vals);
+}
+?>
