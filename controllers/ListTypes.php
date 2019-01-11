890a9073360cbe1f506675c7081f01e4770e35dd
Joe Bordes <joe@tsolucio.com>
2019-01-11 10:09:11 +0100

    cbsr()

--- controllers/ListTypes.php
+++ controllers/ListTypes.php
@@ -9,88 +9,101 @@
  ************************************************************************************/
 class ListTypes_Controller {
 
-	static function process($request) {
+	public static function process($request) {
 		$loginModel = Session_Controller::getLoginContext();
-		
+
 		$client = new Vtiger_WSClient($loginModel->getURL());
 		$login  = $client->doLogin($loginModel->getUsername(), $loginModel->getAccessKey(), $loginModel->getWithPassword());
 
-		if($login) {
-
+		if ($login) {
 			$modules = $client->doListTypes();
 			asort($modules);
 
-			if($modules) {
+			if ($modules) {
 				$modOptions = '';
-				foreach($modules as $module) {
-					$modOptions.= "<option value='".$module['name']."'>".$module['name']."</option>";
+				foreach ($modules as $module) {
+					$modOptions.= "<option value='".$module['name']."'>".$module['name'].'</option>';
 				}
-				foreach($modules as $module) {
+				foreach ($modules as $module) {
 					$desc=$client->doDescribe($module['name']);
-					echo "<div class='row' id='".$module['name']."' style='vertical-align:bottom;'><span class='span5 pull-left'><h3>".$module['name']." (".$desc['label_raw'].'/'.$desc['label'].") </h3></span>";
+					echo "<div class='row' id='".$module['name']."' style='vertical-align:bottom;'><span class='span5 pull-left'><h3>"
+						.$module['name'].' ('.$desc['label_raw'].'/'.$desc['label'].') </h3></span>';
 					echo "<span class='span1 pull-right' style='margin-left:10px;margin-right:60px;margin-top: 30px;'>";
 					echo "<a href='#top'><img src='assets/go_top.png'></a>";
-					echo "</span>";
+					echo '</span>';
 					echo "<span class='span4 pull-right' style='margin-top: 30px;'>";
-					echo "<select onchange=\"\$('.modqaselect').val(this.value);document.location='#'+this.value\" class='small modqaselect'>".$modOptions."</select>";
-					echo "</span>";
-					echo "</div>";
+					echo "<select onchange=\"\$('.modqaselect').val(this.value);document.location='#'+this.value\" class='small modqaselect'>".$modOptions.'</select>';
+					echo '</span>';
+					echo '</div>';
 					echo "<table class='table table-striped small table-condensed'>";
-					echo "<tr><th>REST ID</th><th>Can Create</th><th>Can Update</th><th>Can Delete</th><th>Can Retrieve</th></tr>";
-					echo "<tr>";
+					echo '<tr><th>REST ID</th><th>Can Create</th><th>Can Update</th><th>Can Delete</th><th>Can Retrieve</th></tr>';
+					echo '<tr>';
 					if (is_array($desc)) {
-					echo sprintf("<td nowrap='nowrap'>%s</td>", $desc['idPrefix'].'x');
-					echo sprintf("<td nowrap='nowrap'>%s&nbsp;</td>", $desc['createable']);
-					echo sprintf("<td nowrap='nowrap'>%s&nbsp;</td>", $desc['updateable']);
-					echo sprintf("<td nowrap='nowrap'>%s&nbsp;</td>", $desc['deleteable']);
-					echo sprintf("<td nowrap='nowrap'>%s&nbsp;</td>", $desc['retrieveable']);
-					echo "</tr></table>";
-					echo "<table class='table table-striped small table-condensed'>";
-					echo "<tr><th>Field</th><th>Information</th><th>Block</th><th>Type</th><th width='30%'>Reference/Values</th></tr>";
-					foreach ($desc['fields'] as $field){
-						$fieldname=$field['label'];
-						echo "<tr><td nowrap='nowrap'><b>".$fieldname.'</b><br>Field: '.$field['name'].'<br>Label Raw: '.$field['label_raw'].'</td>';
-						echo "<td>";
-						$fielddesc="Mandatory: ";
-						$fielddesc.=($field['mandatory'] ? 'yes' : 'no')."<br>Null: ".($field['nullable'] ? 'yes' : 'no');
-						$fielddesc.="<br>Editable: ".($field['editable'] ? 'yes' : 'no');
-						if (isset($field['sequence'])) $fielddesc.="<br>Sequence: ".$field['sequence'];
-						echo $fielddesc."</td><td>";
-						if (isset($field['block'])) {
-							$blockddesc="ID: ".$field['block']['blockid'].'<br>';
-							$blockddesc.='Sequence: '.$field['block']['blocksequence'].'<br>';
-							$blockddesc.='Label: '.$field['block']['blocklabel'].'<br>';
-							$blockddesc.='Name: '.$field['block']['blockname'];
-							echo $blockddesc;
+						echo sprintf("<td nowrap='nowrap'>%s</td>", $desc['idPrefix'].'x');
+						echo sprintf("<td nowrap='nowrap'>%s&nbsp;</td>", $desc['createable']);
+						echo sprintf("<td nowrap='nowrap'>%s&nbsp;</td>", $desc['updateable']);
+						echo sprintf("<td nowrap='nowrap'>%s&nbsp;</td>", $desc['deleteable']);
+						echo sprintf("<td nowrap='nowrap'>%s&nbsp;</td>", $desc['retrieveable']);
+						echo '</tr></table>';
+						echo "<table class='table table-striped small table-condensed'>";
+						echo "<tr><th>Field</th><th>Information</th><th>Block</th><th>Type</th><th width='30%'>Reference/Values</th></tr>";
+						foreach ($desc['fields'] as $field) {
+							$fieldname=$field['label'];
+							echo "<tr><td nowrap='nowrap'><b>".$fieldname.'</b><br>Field: '.$field['name'].'<br>Label Raw: '.$field['label_raw'].'</td>';
+							echo '<td>';
+							$fielddesc='Mandatory: ';
+							$fielddesc.=($field['mandatory'] ? 'yes' : 'no').'<br>Null: '.($field['nullable'] ? 'yes' : 'no');
+							$fielddesc.='<br>Editable: '.($field['editable'] ? 'yes' : 'no');
+							if (isset($field['sequence'])) {
+								$fielddesc.='<br>Sequence: '.$field['sequence'];
+							}
+							echo $fielddesc.'</td><td>';
+							if (isset($field['block'])) {
+								$blockddesc='ID: '.$field['block']['blockid'].'<br>';
+								$blockddesc.='Sequence: '.$field['block']['blocksequence'].'<br>';
+								$blockddesc.='Label: '.$field['block']['blocklabel'].'<br>';
+								$blockddesc.='Name: '.$field['block']['blockname'];
+								echo $blockddesc;
+							}
+							echo '</td><td>';
+							$fielddesc='Type: '.$field['type']['name'];
+							if (isset($field['typeofdata'])) {
+								$fielddesc.='&nbsp;('.$field['typeofdata'].')';
+							}
+							if (isset($field['uitype'])) {
+								$fielddesc.='<br>UIType: '.$field['uitype'];
+							}
+							if (isset($field['type']['format'])) {
+								$fielddesc.='<br>Format: '.$field['type']['format'];
+							}
+							if (isset($field['default'])) {
+								$fielddesc.='<br>Default: '.$field['default'];
+							}
+							echo $fielddesc.'</td><td>';
+							$addinfo='';
+							if (isset($field['type']['refersTo'])) {
+								foreach ($field['type']['refersTo'] as $capts) {
+									$addinfo.="$capts, ";
+								}
+							}
+							if (isset($field['type']['picklistValues'])) {
+								foreach ($field['type']['picklistValues'] as $plvn) {
+									$addinfo.=$plvn['value'].', ';
+								}
+							}
+							echo $addinfo.'</td>';
+							echo '</tr>';
 						}
-						echo "</td><td>";
-						$fielddesc="Type: ".$field['type']['name'];
-						if (isset($field['typeofdata'])) $fielddesc.="&nbsp;(".$field['typeofdata'].')';
-						if (isset($field['uitype'])) $fielddesc.="<br>UIType: ".$field['uitype'];
-						if (isset($field['type']['format'])) $fielddesc.="<br>Format: ".$field['type']['format'];
-						if (isset($field['default'])) $fielddesc.="<br>Default: ".$field['default'];
-						echo $fielddesc.'</td><td>';
-						$addinfo='';
-						if (isset($field['type']['refersTo'])) {
-							foreach($field['type']['refersTo'] as $capts) $addinfo.="$capts, ";
-						}
-						if (isset($field['type']['picklistValues'])) {
-							foreach($field['type']['picklistValues'] as $plvs=>$plvn) $addinfo.=$plvn['value'].", ";
-						}
-						echo $addinfo.'</td>';
-						echo "</tr>";
 					}
-					}
-					echo "</table>";
+					echo '</table>';
 				}
 			} else {
 				$lastError = $client->lastError();
-				echo "<div class='alert alert-danger'>ERROR: " . $lastError['message'] . "</div>";
+				echo "<div class='alert alert-danger'>ERROR: " . $lastError['message'] . '</div>';
 			}
 		} else {
 			echo "<div class='alert alert-danger'>ERROR: Login failure!</div>";
 		}
 	}
-
 }
 ?>
