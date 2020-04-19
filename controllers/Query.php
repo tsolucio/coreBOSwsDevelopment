2194a6629d780eb263edca08576f716c35add583
joebordes <joe@tsolucio.com>
2020-04-19 16:42:23 +0200

    cbSR(Query)

--- controllers/Query.php
+++ controllers/Query.php
@@ -10,18 +10,22 @@
  ************************************************************************************/
 class Query_Controller {
 
-	static function process($request) {
-		if(isset($request['q']))
+	public static function process($request) {
+		if (isset($request['q'])) {
 			$query = $request['q'];
-		else
+		} else {
 			$query = '';
-
+		}
 		$escapedQuery = $query;
 		echo "
 		<form method='POST' action='index.php' onsubmit='return validateForm(this);'>
 			<table cellpadding='0' cellspacing='1'>
 				<tr valign=top>
-					<td>Try a query like: select firstname, lastname from Leads order by firstname desc limit 0,2; or <a href='index.php?action=queryExamples'>have a look at our tests and examples.</a><br/><textarea name='q' rows='5' cols='80'>$escapedQuery</textarea></td>
+					<td>
+					Try a query like: select firstname, lastname from Leads order by firstname desc limit 0,2; or <a href='index.php?action=queryExamples'>have a look at our tests and examples.</a>
+					<br/>
+					<textarea name='q' rows='5' cols='80'>$escapedQuery</textarea>
+					</td>
 				</tr>
 				<tr>
 					<td><input type='submit' value='Execute &raquo;' name='__submitButton' class='btn btn-primary btn-large'></td>
@@ -29,47 +33,47 @@ class Query_Controller {
 			</table>
 		</form>";
 
-		if(!empty($query)) {
+		if (!empty($query)) {
 			$loginModel = Session_Controller::getLoginContext();
-			
+
 			$client = new Vtiger_WSClient($loginModel->getURL());
 			$login  = $client->doLogin($loginModel->getUsername(), $loginModel->getAccessKey(), $loginModel->getWithPassword());
 
-			if($login) {
-
+			if ($login) {
 				$result = $client->doQuery($query);
-                                $querynormal = $client->doInvoke("showqueryfromwsdoquery",array("query"=>$query));
-                                echo 'CRM QUERY<pre>'. $querynormal.'</pre>';
-				if(is_array($result)) {
+				$querynormal = $client->doInvoke('showqueryfromwsdoquery', array('query'=>$query));
+				echo 'CRM QUERY<pre>'. $querynormal.'</pre>';
+				if (is_array($result)) {
 					if (count($result)==0) {
 						echo "<div class='alert alert-info'><strong>No results found!</strong></div>";
 					} else {
-					echo "<table cellpadding='3' cellspacing='0' class='table table-striped small'>";
-					$columns = $client->getResultColumns($result);
-					echo "<tr>";
-					foreach($columns as $column) {
-						echo sprintf("<th nowrap='nowrap'>%s</th>", $column);
-					}
-					echo "</tr>";
-					foreach($result as $row) {
-						echo "<tr>";
-						foreach($row as $k => $v) {
-							if($v === '') $v = '&nbsp;';
-							echo sprintf("<td nowrap='nowrap'>%s</td>", $v);
+						echo "<table cellpadding='3' cellspacing='0' class='table table-striped small'>";
+						$columns = $client->getResultColumns($result);
+						echo '<tr>';
+						foreach ($columns as $column) {
+							echo sprintf("<th nowrap='nowrap'>%s</th>", $column);
 						}
-						echo "</tr>";
-					}
-					echo "</table>";
+						echo '</tr>';
+						foreach ($result as $row) {
+							echo '<tr>';
+							foreach ($row as $k => $v) {
+								if ($v === '') {
+									$v = '&nbsp;';
+								}
+								echo sprintf("<td nowrap='nowrap'>%s</td>", $v);
+							}
+							echo '</tr>';
+						}
+						echo '</table>';
 					}
 				} else {
 					$lastError = $client->lastError();
-					echo "<div class='alert alert-danger'><strong>ERROR:</strong> " . $lastError['message'] . "</div>";
+					echo "<div class='alert alert-danger'><strong>ERROR:</strong> " . $lastError['message'] . '</div>';
 				}
 			} else {
 				echo "<div class='alert alert-danger'><strong>ERROR:</strong> Login failure!</div>";
 			}
 		}
-
 	}
 }
 ?>
