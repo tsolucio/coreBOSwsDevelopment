f945f59d2f1cf978a276657e86bc585977f6b55b
Joe Bordes <joe@tsolucio.com>
2024-10-23 13:24:44 +0200

    style and php support

--- controllers/Header.php
+++ controllers/Header.php
@@ -11,19 +11,19 @@
 class Header_Controller {
 
 	public static $title = "coreBOS Webservice Development";
-	
-	static function process($request) {
+
+	public static function process($request) {
 		echo '<!DOCTYPE html PUBLIC "-//W3C//DTD html 4.01 Transitional//EN">';
 		echo "<html>
 			<head>
 				<title>".Header_Controller::$title."</title>
 				<script type='text/javascript'>
 				function validateForm(form) {
-					if(form.q.value == '') return false;
-					
+					if (form.q.value == '') {
+						return false;
+					}
 					form.__submitButton.value = 'Executing';
 					form.__submitButton.disabled = true;
-					
 					return true;
 				}
 				</script>".Header_Controller::getHeaderScripts()."
@@ -32,13 +32,13 @@ class Header_Controller {
 		<div class='container'>
 			<h2>".Header_Controller::$title."</h2>";
 	}
-	
-	static function getHeaderScripts() {
+
+	public static function getHeaderScripts() {
 		return '<script src="assets/jquery.min.js"></script>
 <link rel="stylesheet" href="assets/bootstrap.min.css">';
 	}
 
-	static function getMenu() {
+	public static function getMenu() {
 		$rdo = '<header role="banner" id="top">
 	<nav role="navigation" class="navbar navbar-default" style="margin-bottom: 0;">
 	  <div class="navbar-header">
@@ -53,7 +53,7 @@ class Header_Controller {
 	  <!-- /.navbar-header -->
 	  <div class="collapse navbar-collapse navbar-ex1-collapse">
 	    <ul class="nav navbar-nav navbar-right">';
-		if(Session_Controller::hasLoginContext()) {
+		if (Session_Controller::hasLoginContext()) {
 			$rdo.= '<li><a href="index.php?action=vql">Query</a></li>';
 			$rdo.= '<li><a href="index.php?action=ListTypes">List Types</a></li>';
 			$rdo.= '<li><a href="index.php?action=TestCode">Test Code PHP</a></li>';
@@ -65,7 +65,7 @@ class Header_Controller {
 			<li><a href="http://discussions.corebos.org" target="_blank">Forum</a></li>
 			<li><a href="http://www.corebos.org/post/new-version-coreboswsdev" target="_blank">Blog</a></li>
 			<li><a href="http://corebos.org/page/contact" target="_blank">Contact</a></li>';
-		if(Session_Controller::hasLoginContext()) {
+		if (Session_Controller::hasLoginContext()) {
 			$loginModel = Session_Controller::getLoginContext();
 			$title = 'URL: '.$loginModel->getURL()."          \n";
 			$title.= 'User: '.$loginModel->getUsername().' ('.$loginModel->getUserId().")\n";
@@ -83,5 +83,4 @@ class Header_Controller {
 		return $rdo;
 	}
 }
-
 ?>
